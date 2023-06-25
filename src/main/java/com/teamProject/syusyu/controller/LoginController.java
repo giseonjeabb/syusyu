079f4b10f16b8c45dd1a886693f06239421715e6
chaemin <slaoc0115@gmail.com>
2023-06-25 17:21:03 +0900

    로그인 기능 추가(미완)

--- src/main/java/com/teamProject/syusyu/controller/LoginController.java
+++ src/main/java/com/teamProject/syusyu/controller/LoginController.java
@@ -8,19 +8,20 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.teamProject.syusyu.dao.*;
+import com.teamProject.syusyu.dao.member.MemberDao;
 import com.teamProject.syusyu.domain.*;
+import com.teamProject.syusyu.domain.member.MemberDto;
+import com.teamProject.syusyu.service.MemberService;
+import org.aspectj.apache.bcel.classfile.ConstantNameAndType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
     @Autowired
-    UserDao userDao;
+    MemberService memberService;
 
     @GetMapping("/login")
     public String loginForm() {
@@ -36,19 +37,51 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(String id, String pwd, String toURL, boolean rememberId,
-                        HttpServletRequest request, HttpServletResponse response) throws Exception {
+    @ResponseBody
+    public String login(@RequestBody MemberDto memberDto, HttpServletRequest request) {
+        // 1. id와 pwd를 확인한다.
+        if (!loginCheck(memberDto)) {
+            // 1-1. 일치하지 않으면
+            return "fail";
+        }
+
+        // 2. id와 pwd가 일치하면
+        // 2-1. 세션 객체에 id를 저장한다.
+        HttpSession session = request.getSession();
+        session.setAttribute("id", memberDto.getMemberId());
+
+        // 2-1. 홈으로 이동한다.
+        return "success";
+    }
+
+    private boolean loginCheck(MemberDto memberDto) {
+        int cnt = 0;
+        try {
+            // 만약 반환값이 1이 아니면 false, 1이면 true 반환
+            cnt = memberService.loginCheck(memberDto);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        System.out.println("cnt = " + cnt);
+        return cnt == 1;
+    }
+
+    @PostMapping("/login_bak")
+    public String login_bak(String id, String pwd, String toURL, boolean rememberId,
+                            HttpServletRequest request, HttpServletResponse response) throws Exception {
 
         System.out.println("id = " + id);
         System.out.println("pwd = " + pwd);
 
         // 1. id와 pwd를 확인
-        if (!loginCheck(id, pwd)) {
-            // 2-1   일치하지 않으면, loginForm으로 이동
-            String msg = URLEncoder.encode("id 또는 pwd가 일치하지 않습니다.", "utf-8");
-
-            return "redirect:/login/login?msg=" + msg;
-        }
+//        if (!login_bak(id, pwd)) {
+//            // 2-1   일치하지 않으면, loginForm으로 이동
+//            String msg = URLEncoder.encode("id 또는 pwd가 일치하지 않습니다.", "utf-8");
+//
+//            return "redirect:/login/login?msg=" + msg;
+//        }
         // 2-2. id와 pwd가 일치하면,
         //  세션 객체를 얻어오기
         HttpSession session = request.getSession();
@@ -73,17 +106,5 @@ public class LoginController {
         return "redirect:" + toURL;
     }
 
-    private boolean loginCheck(String id, String pwd) {
-        User user = null;
 
-        try {
-            user = userDao.selectUser(id);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-
-        return user != null && user.getPwd().equals(pwd);
-//        return "asdf".equals(id) && "1234".equals(pwd);
-    }
 }
--- src/main/webapp/WEB-INF/views/include/header.jsp
+++ src/main/webapp/WEB-INF/views/include/header.jsp
@@ -7,13 +7,14 @@
     <title>syusyu</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/static/image2?family=Noto+Sans+KR&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <link rel="stylesheet" href="<c:url value="/css/common/reset.css"/>">
-    <link rel="stylesheet" href="<c:url value="/css/common/common.css"/>">
-    <link rel="stylesheet" href="<c:url value="/css/common/main.css"/>">
+    <link rel="stylesheet" href="<c:url value="/static/css/common/reset.css"/>">
+    <link rel="stylesheet" href="<c:url value="/static/css/common/common.css"/>">
+    <link rel="stylesheet" href="<c:url value="/static/css/common/main.css"/>">
 
+    <script src="https://code.jquery.com/jquery-1.11.3.js"></script>
 </head>
 <body>
 <header id="main_header">
@@ -28,7 +29,7 @@
     </nav>
     <div id="title">
         <div id="logo">
-            <img src="<c:url value="/image/logo/syusyuBlackLogo.png"/>">
+            <img src="<c:url value="/static/image/logo/syusyuBlackLogo.png"/>">
         </div>
         <div id="search_box">
             검색
