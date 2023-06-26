fc6d4fcb9dc04cdd15eaca7cc39a358b95423c16
chaemin <slaoc0115@gmail.com>
2023-06-27 00:29:59 +0900

    Feat: 회원가입 기능 추가

--- src/main/java/com/teamProject/syusyu/controller/member/RegisterController.java
+++ src/main/java/com/teamProject/syusyu/controller/member/RegisterController.java
@@ -1,11 +1,16 @@
 package com.teamProject.syusyu.controller.member;
 
 import com.teamProject.syusyu.common.ViewPath;
+import com.teamProject.syusyu.domain.member.MemberDto;
+import com.teamProject.syusyu.domain.member.MemberValidator;
 import com.teamProject.syusyu.service.member.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Validator;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/register")
@@ -13,7 +18,15 @@ public class RegisterController {
     @Autowired
     MemberService memberService;
 
-    @GetMapping("/register")
+    @InitBinder
+    public void toDate(WebDataBinder binder) {
+        // UserValidator를 생성해서 WebDataBinder에 등록
+        binder.setValidator(new MemberValidator()); // UserValidator를 WebDataBinder의 로컬 validator(현재 컨트롤러 내에서만 사용 가능)로 등록
+        List<Validator> validatorList = binder.getValidators(); // validator 등록된 거 확인
+        System.out.println("validatorList = " + validatorList);
+    }
+
+    @GetMapping("/add")
     public String registerForm() {
         return ViewPath.MEMBER + "registerForm";
     }
@@ -24,4 +37,20 @@ public class RegisterController {
     * 매개변수 :
     * 반환타입 :
     * */
+    @PostMapping("/add")
+    @ResponseBody
+    public String register(@RequestBody MemberDto memberDto) {
+        try {
+            boolean result = memberService.register(memberDto);
+
+            if (!result)
+                throw new Exception("register failed");
+
+            return "success";
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "fail";
+        }
+    }
 }
---
+++ src/main/java/com/teamProject/syusyu/domain/member/MemberValidator.java
@@ -0,0 +1,32 @@
+package com.teamProject.syusyu.domain.member;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class MemberValidator implements Validator {
+    @Override
+    // 이 검증기로 검증 가능한 객체인지 알려주는 메서드
+    public boolean supports(Class<?> aClass) {
+        return MemberDto.class.equals(aClass);
+    }
+
+    @Override
+    // 객체를 검증하는 메서드 - target : 검증할 객체, errors : 검증 시 발생한 에러 저장소
+    public void validate(Object target, Errors errors) {
+        System.out.println("MemberDtoValidator.validate() is called");
+        MemberDto memberDto = (MemberDto) target;
+        String id = memberDto.getLginId();
+
+        // MemberDto객체를 검증한다는 건 이런 것. id가 null인지, 빈문자열인지 검증
+        if (id == null || "".equals(id.trim())) {
+            errors.rejectValue("id", "required"); // 필수 값인데 저장 안 했다고 에러를 저장
+        }
+        // 비어있거나 공백 or 탭인 경우 errors 객체에 필드 이름은 id, 에러코드는 required로 해서 저장해라.
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "pwd", "required");
+
+        if (id == null || id.length() < 5 || id.length() > 12) {
+            errors.rejectValue("id", "invalidLength", new String[]{"5", "12"}, null);
+        }
+    }
+}
