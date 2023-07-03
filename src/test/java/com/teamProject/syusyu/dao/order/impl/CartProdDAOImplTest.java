ff06ed55c4699ee783da196258c56c6f8d49a150
chaemin <slaoc0115@gmail.com>
2023-07-03 09:22:04 +0900

    Test: CartProdDAO(장바구니상품) 테스트 코드 추가  - 조회, 수정, 삭제, 추가

---
+++ src/test/java/com/teamProject/syusyu/dao/order/impl/CartProdDAOImplTest.java
@@ -0,0 +1,84 @@
+package com.teamProject.syusyu.dao.order.impl;
+
+import com.teamProject.syusyu.dao.order.CartProdDAO;
+import com.teamProject.syusyu.domain.order.CartProductDTO;
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.stream.Collectors;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+public class CartProdDAOImplTest extends TestCase {
+    @Autowired
+    CartProdDAO cartProdDAO;
+
+    @Before
+    public void beforeEach() throws Exception {
+        cartProdDAO.deleteAll();
+        assertEquals(0, cartProdDAO.select(80001).size());
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+        // 장바구니를 하나 추가한다.
+        CartProductDTO cartProductDTO = new CartProductDTO(1, 10001, 3, 1, 80001);
+        cartProdDAO.insert(cartProductDTO);
+        assertEquals(1, cartProdDAO.select(80001).size());
+
+        CartProductDTO cartProductDTO2 = new CartProductDTO(1, 10007, 2, null, 80001);
+        cartProdDAO.insert(cartProductDTO2);
+        assertEquals(2, cartProdDAO.select(80001).size());
+    }
+
+    @Test
+    public void testSelect() throws Exception {
+        CartProductDTO cartProductDTO = new CartProductDTO(1, 10001, 3, 1, 80001);
+        cartProdDAO.insert(cartProductDTO);
+
+        assertEquals(0, cartProdDAO.select(80001).size());
+        CartProductDTO result = cartProdDAO.select(80001).get(0);
+
+//        int price = result.getPrice();
+//        int dcPer = result.getd
+//
+//        assertTrue(result);
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        CartProductDTO cartProductDTO = new CartProductDTO(1, 10001, 3, 1, 80001);
+        cartProdDAO.insert(cartProductDTO);
+        assertEquals(1, cartProdDAO.select(80001).size());
+
+        cartProductDTO.setQty(2);
+        cartProdDAO.update(cartProductDTO);
+
+        CartProductDTO result = cartProdDAO.select(80001)
+                .stream()
+                .filter(cartProductDTO1 -> cartProductDTO.getCartProdNo() == cartProductDTO.getCartProdNo())
+                .collect(Collectors.toList())
+                .get(0);
+
+        System.out.println("cartProductDTO = " + cartProductDTO); // 내가 생성해서 update한 객체
+        System.out.println("result = " + result);
+
+        assertEquals(result, cartProductDTO);
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        CartProductDTO cartProductDTO = new CartProductDTO(1, 10001, 3, 1, 80001);
+        cartProdDAO.insert(cartProductDTO);
+        assertEquals(1, cartProdDAO.select(80001).size());
+
+        cartProdDAO.delete(new int[]{cartProductDTO.getCartProdNo()});
+
+        assertEquals(0, cartProdDAO.select(80001).size());
+    }
+}
