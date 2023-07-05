f25665984890ae0770b585c902db65f29db10637
gi-dor <rltjs3563@naver.com>
2023-07-05 20:09:48 +0900

    "Docs : NoticeController 쓰지 않는 기능제거
    고객이 보는 앞 화면 이라서
    write , modify , remove 기능 제거"

--- src/main/java/com/teamProject/syusyu/controller/cs/NoticeController.java
+++ src/main/java/com/teamProject/syusyu/controller/cs/NoticeController.java
@@ -1,6 +1,5 @@
 package com.teamProject.syusyu.controller.cs;
 import com.teamProject.syusyu.common.ViewPath;
-import com.teamProject.syusyu.dao.cs.NoticeDAO;
 import com.teamProject.syusyu.domain.cs.NoticeDTO;
 import com.teamProject.syusyu.domain.cs.PageHandler;
 import com.teamProject.syusyu.domain.cs.SearchCondition;
@@ -9,9 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
@@ -24,8 +26,90 @@ public class NoticeController {
     @Autowired
     NoticeService noticeService;
 
-    @Autowired
-    NoticeDAO noticeDao;
+
+
+    @PostMapping("/modify")
+    public String modify(NoticeDTO noticeDto, HttpSession session, Model m, RedirectAttributes rattr) {
+        int writer = (int) session.getAttribute("id");
+        noticeDto.setRegrId(writer);
+//        memberDto.setLginId(writer);
+        try {
+            int rowCnt = noticeService.modify(noticeDto);
+
+            if (rowCnt != 1)
+                throw new Exception("Modify failed");
+
+            rattr.addFlashAttribute("msg", "MOD_OK");
+
+
+            return "redirect:/notice/noticeList";
+
+             } catch (Exception e) {
+                        e.printStackTrace();
+                        m.addAttribute("noticeDto", noticeDto);
+                        m.addAttribute("msg", "MOD_ERR");
+
+                         return ViewPath.MYPAGE+"notice";
+                }
+            }
+
+
+    @PostMapping("/write")
+    public String write(NoticeDTO noticeDto, HttpSession session, Model m, RedirectAttributes rattr) {
+        int writer = (int) session.getAttribute("id");
+        noticeDto.setRegrId(writer);
+
+        try {
+            int rowCnt = noticeService.write(noticeDto);
+
+            if (rowCnt != 1)
+                throw new Exception("write failed");
+
+            rattr.addFlashAttribute("msg", "WRT_OK");
+
+            return "redirect:/notice/noticeList";
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            m.addAttribute("noticeDto", noticeDto);
+            m.addAttribute("msg", "WRT_ERR");
+
+            return ViewPath.MYPAGE+"notice";
+
+        }
+
+    }
+
+
+    @GetMapping("/write")
+    public String write(Model m) {
+        m.addAttribute("mode", "new");
+
+        return ViewPath.CS+"notice"; // 읽기와 쓰기에 사용 , 쓰기에 사용할때는 mode = new
+    }
+
+
+    @PostMapping("/remove")
+    public String remove(Integer notcNo, Integer page, Integer pageSize, Model m, HttpSession session, RedirectAttributes rattr) {
+        String writer = (String) session.getAttribute("id");
+        try {
+            m.addAttribute("page", page);
+            m.addAttribute("pageSize", pageSize);
+
+            int rowCnt = noticeService.remove(notcNo, writer);
+
+            if (rowCnt != 1)
+                throw new Exception("board remove error");
+
+            rattr.addFlashAttribute("msg", "DEL_OK");
+        } catch (Exception e) {
+            e.printStackTrace();
+            rattr.addFlashAttribute("msg", "DEL_ERR");
+        }
+
+        return "redirect:/notice/noticeList";
+
+    }
 
 
     @GetMapping("/read")
@@ -56,8 +140,10 @@ public class NoticeController {
     @GetMapping("/noticeList")
     public String list(SearchCondition sc, Model m, HttpServletRequest request) {
 
+
 //        if (!loginCheck(request))
 //            return "redirect:/login/login?toURL=" + request.getRequestURL();  // 로그인을 안했으면 로그인 화면으로 이동
+
 
         try {
             // 페이지 핸들러  << < 1,2,3,4,5,6,7,8,9,10 > >>
@@ -78,11 +164,21 @@ public class NoticeController {
 
         } catch (Exception e) {
             e.printStackTrace();
-            m.addAttribute("msg", "LIST_ERROR");
+            m.addAttribute("msg", "LIST_ERR");
             m.addAttribute("totalCnt", 0);
         }
 
         return ViewPath.MYPAGE+"noticeList"; // 로그인을 한 상태이면, 공지사항 화면으로 이동
 
+
     }
+
+//    private boolean loginCheck(HttpServletRequest request) {
+//        // 1. 세션을 얻어서
+//        HttpSession session = request.getSession();
+//        // 2. 세션에 id가 있는지 확인, 있으면 true를 반환
+//        return session.getAttribute("id") != null;
+//    }
+
+
 }
