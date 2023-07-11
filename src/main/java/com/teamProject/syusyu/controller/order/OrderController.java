f878c040193ef14f2a1686de17465e1032105176
chaemin <slaoc0115@gmail.com>
2023-07-11 17:48:22 +0900

    Feat: 주문 생성 API 구현

--- src/main/java/com/teamProject/syusyu/controller/order/OrderController.java
+++ src/main/java/com/teamProject/syusyu/controller/order/OrderController.java
@@ -1,13 +1,17 @@
 package com.teamProject.syusyu.controller.order;
 
 import com.teamProject.syusyu.common.ViewPath;
+import com.teamProject.syusyu.domain.order.*;
 import com.teamProject.syusyu.service.order.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -23,10 +27,10 @@ public class OrderController {
      * 주문서에 필요한 정보들을 조회해서 주문서 화면에 넘겨준다.
      *
      * @param cartProdNoArr 주문할 장바구니상품아이디 배열
-     * @param mbrId 장바구니를 조회할 사용자의 아이디
+     * @param mbrId         장바구니를 조회할 사용자의 아이디
      * @return 주문서에 필요한 정보들(주문상품정보, 주문자정보, 배송지정보, 할인정보)
      * @author min
-     * @since  2023/07/07
+     * @since 2023/07/07
      */
     @GetMapping("/orderSheet")
     public String orderSheet(Model m, int[] cartProdNoArr, @SessionAttribute int mbrId) {
@@ -43,5 +47,20 @@ public class OrderController {
         }
 
         return ViewPath.FOS_ORDER + "orderSheet";
+    }
+
+    /**
+     * 주문을 생성한다.
+     *
+     * @param
+     * @return
+     * @throws
+     * @author min
+     * @since 2023/07/11
+     */
+    public String order(@RequestBody OrderRequestDTO orderRequestDTO, @SessionAttribute int mbrId) throws Exception {
+        Order order = new Order(orderRequestDTO, mbrId);
+        service.order(order);
+        return null;
     }
 }
--- src/main/java/com/teamProject/syusyu/service/order/OrderService.java
+++ src/main/java/com/teamProject/syusyu/service/order/OrderService.java
@@ -1,7 +1,14 @@
 package com.teamProject.syusyu.service.order;
 
+import com.teamProject.syusyu.domain.order.*;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 import java.util.Map;
 
 public interface OrderService {
     Map<String, Object> orderSheet(int[] cartProdNoArr, int mbrId) throws Exception;
+
+    @Transactional(rollbackFor = Exception.class)
+    String order(Order order) throws Exception;
 }
