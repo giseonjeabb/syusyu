82decfb5571ff055c96ad53b5afc9d289738dfaf
gi-dor <rltjs3563@naver.com>
2023-07-14 15:11:34 +0900

    "Feat: FAQ 페이징,검색
       FAQ 글 페이지 처리 , FAQ 검색바 기능 "

--- src/main/java/com/teamProject/syusyu/domain/cs/FaqSearchCondition.java
+++ src/main/java/com/teamProject/syusyu/domain/cs/FaqSearchCondition.java
@@ -4,16 +4,18 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 public class FaqSearchCondition {
 
+    // 검색 에 필요한 클래스
+
     private String keyword = ""; // 검색어
     private String option = "";  // 검색 종류
+    private String key = ""; // FAQ 타입 종류 검색
 
+    public FaqSearchCondition() {}
 
-    public FaqSearchCondition(){}
-
-
-    public FaqSearchCondition( String keyword, String option) {
+    public FaqSearchCondition(String keyword, String option, String key) {
         this.keyword = keyword;
         this.option = option;
+        this.key = key;
     }
 
     public String getKeyword() {
@@ -32,11 +34,20 @@ public class FaqSearchCondition {
         this.option = option;
     }
 
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
     @Override
     public String toString() {
         return "FaqSearchCondition{" +
                 "keyword='" + keyword + '\'' +
                 ", option='" + option + '\'' +
+                ", key='" + key + '\'' +
                 '}';
     }
 
