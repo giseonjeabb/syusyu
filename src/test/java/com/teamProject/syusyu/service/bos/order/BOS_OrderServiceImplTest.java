4dbb08c4c1a86e79f22f387c35f6a9329878cb41
chaemin <slaoc0115@gmail.com>
2023-07-25 14:59:11 +0900

    Test: 관리자 주문조회 - 주문확인 기능 테스트 코드 추가

--- src/test/java/com/teamProject/syusyu/service/bos/order/BOS_OrderServiceImplTest.java
+++ src/test/java/com/teamProject/syusyu/service/bos/order/BOS_OrderServiceImplTest.java
@@ -1,5 +1,7 @@
 package com.teamProject.syusyu.service.bos.order;
 
+import com.teamProject.syusyu.dao.order.OrdDtlDAO;
+import com.teamProject.syusyu.domain.order.OrdDtlDTO;
 import com.teamProject.syusyu.domain.order.OrderInfoDTO;
 import com.teamProject.syusyu.domain.order.request.OrderSearchRequestDTO;
 import org.junit.Test;
@@ -8,13 +10,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
 public class BOS_OrderServiceImplTest {
     @Autowired
     BOS_OrderService service;
+
+    @Autowired
+    OrdDtlDAO ordDtlDAO;
 
     @Test
     public void getOrderList() throws Exception {
@@ -28,5 +36,19 @@ public class BOS_OrderServiceImplTest {
         List<OrderInfoDTO> orderInfoDTOList = service.getOrderList(orderSearchRequestDTO);
         System.out.println("orderInfoDTOList = " + orderInfoDTOList);
 
+    }
+
+    @Test
+    public void confirmOrderTest() throws Exception {
+        Integer[] ordDtlNoArr = new Integer[]{246, 247, 251, 255, 261};
+        List<Integer> ordDtlNoList = Arrays.asList(ordDtlNoArr);
+        service.confirmOrder(ordDtlNoList, 80001);
+
+        for (Integer ordDtlNo : ordDtlNoArr) {
+            OrdDtlDTO ordDtlDTO = ordDtlDAO.selectOrderDetail(ordDtlNo);
+            assertEquals(ordDtlDTO.getOrdStus(), "20");
+            assertNotNull(ordDtlDTO.getUpdDttm());
+            assertNotNull(ordDtlDTO.getUpdrId());
+        }
     }
 }
