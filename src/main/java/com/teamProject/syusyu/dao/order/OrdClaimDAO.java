37a407281396dbacd6a2dda7611d8eaa105a4c41
chaemin <slaoc0115@gmail.com>
2023-07-30 16:49:32 +0900

    Feat: 주문취소 - 주문 클레임 insert 쿼리, DAO, 테스트 코드 추가

---
+++ src/main/java/com/teamProject/syusyu/dao/order/OrdClaimDAO.java
@@ -0,0 +1,17 @@
+package com.teamProject.syusyu.dao.order;
+
+import com.teamProject.syusyu.domain.order.OrdClaimDTO;
+
+import java.util.List;
+
+public interface OrdClaimDAO {
+    int insertCancelClaim(OrdClaimDTO ordClaimDTO) throws Exception;
+
+    OrdClaimDTO selectOrdClaim(int ordClaimNo) throws Exception;
+
+    int deleteOrdClaim(int ordClaimNo) throws Exception;
+
+    int countOrdClaim() throws Exception;
+
+    List<OrdClaimDTO> selectAllOrdClaims() throws Exception;
+}
