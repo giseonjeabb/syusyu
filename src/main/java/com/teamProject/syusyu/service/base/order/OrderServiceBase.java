5ce83b4bb48793d720ae33a4b04c0c353f98ab22
chaemin <slaoc0115@gmail.com>
2023-07-31 12:20:23 +0900

    Refactor: 주문상태 변경 및 이력 추가 처리 로직 개선
    기존 : 주문 상태 변경 및 이력 추가 일괄적으로 처리
    변경 : 각 주문에 대해 순차적으로 상태 변경 및 이력 추가가 되도록 로직 개선
    => 주문 상태 변경과 이력 추가가 주문 건별로 개별적으로 이루어지며 각각의 주문 처리에 대해 명확해지도록 수정함

--- src/main/java/com/teamProject/syusyu/service/base/order/OrderServiceBase.java
+++ src/main/java/com/teamProject/syusyu/service/base/order/OrderServiceBase.java
@@ -18,72 +18,78 @@ public abstract class OrderServiceBase {
     private OrdStusHistDAO ordStusHistDAO;
 
     /**
-     * 주문 건의 주문상태를 업데이트하고 주문상태이력을 추가한다.
+     * 각 주문의 주문상태를 업데이트하고 주문상태이력을 추가한다.
+     * 각 주문에 대해서 순차적으로 주문상태를 업데이트하고 주문상태이력을 추가한다.
      *
      * @param ordDtlNoList 주문상태를 업데이트할 주문상세번호를 담은 리스트
      * @param mbrId        사용자 ID
      * @param ordStus      변경될 주문상태
      * @throws Exception 주문상태 업데이트 또는 주문상태이력 추가 도중 발생할 수 있는 예외
      * @author min
-     * @since 2023/07/25
+     * @since 2023/07/31
      */
     @Transactional(rollbackFor = Exception.class)
     public void processUpdateOrderStatus(List<Integer> ordDtlNoList, int mbrId, String ordStus) throws Exception {
-        int updatedRows = updateOrderStatus(ordDtlNoList, mbrId, ordStus); // 1. 주문 상태 업데이트
-        int insertedRows = addOrderStatusHistory(ordDtlNoList, mbrId, ordStus); // 2. 주문 상태 이력 추가
+        for (Integer ordDtlNo : ordDtlNoList) {
+            boolean isSuccess = updateOrderStatusAndAddHistory(ordDtlNo, mbrId, ordStus);
 
-        if (updatedRows != ordDtlNoList.size() || insertedRows != ordDtlNoList.size())
-            throw new Exception("주문 상태 변경 처리에 실패하였습니다. 업데이트된 주문의 수 또는 추가된 주문 이력의 수가 전체 주문의 수와 일치하지 않습니다.");
-
+            if (!isSuccess)
+                throw new Exception("주문 번호 " + ordDtlNo + "의 주문 상태 변경 처리에 실패하였습니다. 주문 상태가 변경되거나 추가된 주문 이력의 수가 1이 아닙니다.");
+        }
     }
+
+    /**
+     * 주문 상태를 업데이트하고 주문 상태 이력을 추가한다.
+     *
+     * @param ordDtlNo 주문상태를 업데이트할 주문의 주문상세번호
+     * @param mbrId    사용자 ID
+     * @param ordStus  변경될 주문상태
+     * @return 주문 상태 변경 및 이력 추가 성공 여부
+     * @throws Exception 주문 상태 업데이트 또는 주문 상태 이력 추가 도중 발생할 수 있는 예외
+     * @author min
+     * @since 2023/07/31
+     */
+    public boolean updateOrderStatusAndAddHistory(int ordDtlNo, int mbrId, String ordStus) throws Exception {
+        int updatedRow = updateOrderStatus(ordDtlNo, mbrId, ordStus); // 1. 주문 상태 업데이트
+        int insertedRow = addOrderStatusHistory(ordDtlNo, mbrId, ordStus); // 2. 주문 상태 이력 추가
+
+        return updatedRow == 1 && insertedRow == 1;
+    }
+
 
     /**
      * 주문 건의 주문상태를 주어진 주문상태로 변경한다.
      *
-     * @param ordDtlNoList 주문상태를 업데이트할 주문의 주문상세번호를 담은 리스트
-     * @param mbrId        사용자 ID
-     * @param ordStus      변경될 주문상태
+     * @param ordDtlNo 주문상태를 업데이트할 주문의 주문상세번호
+     * @param mbrId    사용자 ID
+     * @param ordStus  변경될 주문상태
      * @return 업데이트된 주문의 수
      * @throws Exception 주문 상태 업데이트 도중 발생할 수 있는 예외
      * @author min
-     * @since 2023/07/25
+     * @since 2023/07/31
      */
-    public int updateOrderStatus(List<Integer> ordDtlNoList, int mbrId, String ordStus) throws Exception {
+    public int updateOrderStatus(int ordDtlNo, int mbrId, String ordStus) throws Exception {
         Map<String, Object> map = new HashMap<>();
-        int updatedRows = 0;
+        map.put("ordDtlNo", ordDtlNo);
+        map.put("ordStus", ordStus);
+        map.put("updrId", mbrId);
 
-        for (Integer ordDtlNo : ordDtlNoList) {
-            map.clear();
-            map.put("ordDtlNo", ordDtlNo);
-            map.put("ordStus", ordStus); // 20 = 주문확인
-            map.put("updrId", mbrId);
-
-            updatedRows += ordDtlDAO.updateOrdStus(map);
-        }
-
-        return updatedRows;
+        return ordDtlDAO.updateOrdStus(map);
     }
 
     /**
-     * 주어진 주문상세번호 리스트에 해당하는 주문상태이력을 추가한다.
+     * 주어진 주문상세번호에 해당하는 주문상태이력을 추가한다.
      *
-     * @param ordDtlNoList 주문상태이력을 추가할 주문의 주문상세번호를 담은 리스트
-     * @param mbrId        사용자 ID
-     * @param ordStus      변경될 주문상태
+     * @param ordDtlNo 주문상태이력을 추가할 주문의 주문상세번호
+     * @param mbrId    사용자 ID
+     * @param ordStus  변경될 주문상태
      * @return 추가된 주문 상태 이력의 수
      * @throws Exception 주문 상태 이력 추가 도중 발생할 수 있는 예외
      * @author min
-     * @since 2023/07/25
+     * @since 2023/07/31
      */
-    public int addOrderStatusHistory(List<Integer> ordDtlNoList, int mbrId, String ordStus) throws Exception {
-        OrdStusHistDTO ordStusHistDTO;
-        int insertedRows = 0;
-
-        for (Integer ordDtlNo : ordDtlNoList) {
-            ordStusHistDTO = new OrdStusHistDTO(ordDtlNo, ordStus, null, mbrId);
-            insertedRows += ordStusHistDAO.insertOrderStatusHistory(ordStusHistDTO);
-        }
-
-        return insertedRows;
+    public int addOrderStatusHistory(int ordDtlNo, int mbrId, String ordStus) throws Exception {
+        OrdStusHistDTO ordStusHistDTO = new OrdStusHistDTO(ordDtlNo, ordStus, null, mbrId);
+        return ordStusHistDAO.insertOrderStatusHistory(ordStusHistDTO);
     }
 }
