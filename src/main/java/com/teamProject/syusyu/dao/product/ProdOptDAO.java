222d337a2cbd91446c50843ff837878c59bf86d6
soso <lks1331@gmail.com>
2023-07-31 13:41:30 +0900

    Feat: price, smlImg, opt, product DAO추가

--- src/main/java/com/teamProject/syusyu/dao/product/ProdOptDAO.java
+++ src/main/java/com/teamProject/syusyu/dao/product/ProdOptDAO.java
@@ -1,9 +1,18 @@
 package com.teamProject.syusyu.dao.product;
 
 import com.teamProject.syusyu.domain.product.ProdOptDTO;
+import com.teamProject.syusyu.domain.product.ProductDTO;
 
 import java.util.List;
 
 public interface ProdOptDAO {
     List<ProdOptDTO> selectProdOptSizeList(int prodId);
+
+    int insertOptGrp(ProductDTO productDTO) throws Exception;
+
+    int insertOptItem(ProductDTO productDTO) throws Exception;
+
+    int insertProdOpt(ProductDTO productDTO) throws Exception;
+
+    int insertProdOptComb(ProductDTO productDTO) throws Exception;
 }
