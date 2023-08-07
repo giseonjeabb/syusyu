a0a1ad2ac1b0a5372b0db3840d740ee8b27fb1c4
gi-dor <rltjs3563@naver.com>
2023-08-07 13:39:40 +0900

    Feat : 관리자 - 상품후기 페이지 - 미완성
    list() 만 구현되어있음.

--- src/main/java/com/teamProject/syusyu/domain/product/ReviewDTO.java
+++ src/main/java/com/teamProject/syusyu/domain/product/ReviewDTO.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import java.util.Objects;
 
 public class ReviewDTO {
+    private String brndKoNm;
+    private String prodNm;
     private Integer revwNo;
     private Integer prodId;
     private BigDecimal starRating;
@@ -21,13 +23,15 @@ public class ReviewDTO {
     private Date delDttm;
     private Integer delrId;
     private String delYn;
+    private String imageList;
 
 
+    public ReviewDTO(){}
 
 
-    public ReviewDTO() {}
-
-    public ReviewDTO(Integer revwNo, Integer prodId, BigDecimal starRating, Integer height, BigDecimal weight, Integer shoeSize, Integer likeCnt, String revwCn, Date regDttm, String lginId, Integer regrId, Date updDttm, Integer updrId, Date delDttm, Integer delrId, String delYn) {
+    public ReviewDTO(String brndKoNm, String prodNm, Integer revwNo, Integer prodId, BigDecimal starRating, Integer height, BigDecimal weight, Integer shoeSize, Integer likeCnt, String revwCn, Date regDttm, String lginId, Integer regrId, Date updDttm, Integer updrId, Date delDttm, Integer delrId, String delYn, String imageList) {
+        this.brndKoNm = brndKoNm;
+        this.prodNm = prodNm;
         this.revwNo = revwNo;
         this.prodId = prodId;
         this.starRating = starRating;
@@ -44,41 +48,23 @@ public class ReviewDTO {
         this.delDttm = delDttm;
         this.delrId = delrId;
         this.delYn = delYn;
+        this.imageList = imageList;
     }
 
-    @Override
-    public String toString() {
-        return "ReviewDTO{" +
-                "revwNo=" + revwNo +
-                ", prodId=" + prodId +
-                ", starRating=" + starRating +
-                ", height=" + height +
-                ", weight=" + weight +
-                ", shoeSize=" + shoeSize +
-                ", likeCnt=" + likeCnt +
-                ", revwCn='" + revwCn + '\'' +
-                ", regDttm=" + regDttm +
-                ", lginId='" + lginId + '\'' +
-                ", regrId=" + regrId +
-                ", updDttm=" + updDttm +
-                ", updrId=" + updrId +
-                ", delDttm=" + delDttm +
-                ", delrId=" + delrId +
-                ", delYn='" + delYn + '\'' +
-                '}';
+    public String getBrndKoNm() {
+        return brndKoNm;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ReviewDTO reviewDTO = (ReviewDTO) o;
-        return Objects.equals(revwNo, reviewDTO.revwNo) && Objects.equals(prodId, reviewDTO.prodId) && Objects.equals(starRating, reviewDTO.starRating) && Objects.equals(height, reviewDTO.height) && Objects.equals(weight, reviewDTO.weight) && Objects.equals(shoeSize, reviewDTO.shoeSize) && Objects.equals(likeCnt, reviewDTO.likeCnt) && Objects.equals(revwCn, reviewDTO.revwCn) && Objects.equals(regDttm, reviewDTO.regDttm) && Objects.equals(lginId, reviewDTO.lginId) && Objects.equals(regrId, reviewDTO.regrId) && Objects.equals(updDttm, reviewDTO.updDttm) && Objects.equals(updrId, reviewDTO.updrId) && Objects.equals(delDttm, reviewDTO.delDttm) && Objects.equals(delrId, reviewDTO.delrId) && Objects.equals(delYn, reviewDTO.delYn);
+    public void setBrndKoNm(String brndKoNm) {
+        this.brndKoNm = brndKoNm;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(revwNo, prodId, starRating, height, weight, shoeSize, likeCnt, revwCn, regDttm, lginId, regrId, updDttm, updrId, delDttm, delrId, delYn);
+    public String getProdNm() {
+        return prodNm;
+    }
+
+    public void setProdNm(String prodNm) {
+        this.prodNm = prodNm;
     }
 
     public Integer getRevwNo() {
@@ -207,5 +193,51 @@ public class ReviewDTO {
 
     public void setDelYn(String delYn) {
         this.delYn = delYn;
+    }
+
+    public String getImageList() {
+        return imageList;
+    }
+
+    public void setImageList(String imageList) {
+        this.imageList = imageList;
+    }
+
+    @Override
+    public String toString() {
+        return "ReviewDTO{" +
+                "brndKoNm='" + brndKoNm + '\'' +
+                ", prodNm='" + prodNm + '\'' +
+                ", revwNo=" + revwNo +
+                ", prodId=" + prodId +
+                ", starRating=" + starRating +
+                ", height=" + height +
+                ", weight=" + weight +
+                ", shoeSize=" + shoeSize +
+                ", likeCnt=" + likeCnt +
+                ", revwCn='" + revwCn + '\'' +
+                ", regDttm=" + regDttm +
+                ", lginId='" + lginId + '\'' +
+                ", regrId=" + regrId +
+                ", updDttm=" + updDttm +
+                ", updrId=" + updrId +
+                ", delDttm=" + delDttm +
+                ", delrId=" + delrId +
+                ", delYn='" + delYn + '\'' +
+                ", imageList='" + imageList + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ReviewDTO reviewDTO = (ReviewDTO) o;
+        return Objects.equals(brndKoNm, reviewDTO.brndKoNm) && Objects.equals(prodNm, reviewDTO.prodNm) && Objects.equals(revwNo, reviewDTO.revwNo) && Objects.equals(prodId, reviewDTO.prodId) && Objects.equals(starRating, reviewDTO.starRating) && Objects.equals(height, reviewDTO.height) && Objects.equals(weight, reviewDTO.weight) && Objects.equals(shoeSize, reviewDTO.shoeSize) && Objects.equals(likeCnt, reviewDTO.likeCnt) && Objects.equals(revwCn, reviewDTO.revwCn) && Objects.equals(regDttm, reviewDTO.regDttm) && Objects.equals(lginId, reviewDTO.lginId) && Objects.equals(regrId, reviewDTO.regrId) && Objects.equals(updDttm, reviewDTO.updDttm) && Objects.equals(updrId, reviewDTO.updrId) && Objects.equals(delDttm, reviewDTO.delDttm) && Objects.equals(delrId, reviewDTO.delrId) && Objects.equals(delYn, reviewDTO.delYn) && Objects.equals(imageList, reviewDTO.imageList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brndKoNm, prodNm, revwNo, prodId, starRating, height, weight, shoeSize, likeCnt, revwCn, regDttm, lginId, regrId, updDttm, updrId, delDttm, delrId, delYn, imageList);
     }
 }
